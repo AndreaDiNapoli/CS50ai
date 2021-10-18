0ff96dc5cd8f716f9741d9640b1c2f9f85f7450f
AndreaDiNapoli <andrea.dinapoli.90@gmail.com>
2021-10-18 17:47:47 +0200

    Knight completed

--- knights/puzzle.py
+++ knights/puzzle.py
@@ -12,21 +12,59 @@ CKnave = Symbol("C is a Knave")
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    # Encode information about exclusivity of Knight and Knave "classes" and that every char is either Knight or Knave
+    Or(AKnight, AKnave),
+    Or(BKnight, BKnave),
+    Or(CKnight, CKnave),
+    Not(And(AKnight, AKnave)),
+    Not(And(BKnight, BKnave)),
+    Not(And(CKnight, CKnave)),
+
+    # Encode the knowledge that if A is Knight, what he said it's true. Else, it's false. 
+    # A said: "And(AKnight, AKnave)"
+    Implication(AKnight, And(AKnight, AKnave)),
+    Implication(AKnave, Not(And(AKnight, AKnave)))
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+    # Encode information about exclusivity of Knight and Knave "classes"
+    Or(AKnight, AKnave),
+    Or(BKnight, BKnave),
+    Or(CKnight, CKnave),
+    Not(And(AKnight, AKnave)),
+    Not(And(BKnight, BKnave)),
+    Not(And(CKnight, CKnave)),
+    
+    # Encode the knowledge that if A is Knight, what he said it's true. Else, it's false. 
+    # A said: "And(AKnave, BKnaves)"
+    Implication(AKnight, And(AKnave, BKnave)),
+    Implication(AKnave, Not(And(AKnave, BKnave)))
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+    # Encode information about exclusivity of Knight and Knave "classes"
+    Or(AKnight, AKnave),
+    Or(BKnight, BKnave),
+    Or(CKnight, CKnave),
+    Not(And(AKnight, AKnave)),
+    Not(And(BKnight, BKnave)),
+    Not(And(CKnight, CKnave)),
+
+    # Encode the knowledge that if A is Knight, what he said it's true. Else, it's false. 
+    # A said: "And(Biconditional(AKnight, BKnight), Biconditional(AKnave, BKnave))"
+    Implication(AKnight, And(Biconditional(AKnight, BKnight), Biconditional(AKnave, BKnave))),
+    Implication(AKnave, Not(And(Biconditional(AKnight, BKnight), Biconditional(AKnave, BKnave)))),
+
+    # Encode the knowledge that if B is Knight, what he said it's true. Else, it's false. 
+    # B said: "And(Implication(AKnight, BKnave), Implication(AKnave, BKnight))"
+    Implication(BKnight, And(Implication(AKnight, BKnave), Implication(AKnave, BKnight))),
+    Implication(BKnave, Not(And(Implication(AKnight, BKnave), Implication(AKnave, BKnight))))
 )
 
 # Puzzle 3
@@ -35,9 +73,40 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    # TODO
-)
+    # Encode information about exclusivity of Knight and Knave "classes"
+    Or(AKnight, AKnave),
+    Or(BKnight, BKnave),
+    Or(CKnight, CKnave),
+    Not(And(AKnight, AKnave)),
+    Not(And(BKnight, BKnave)),
+    Not(And(CKnight, CKnave)),
 
+    # Encode the knowledge that if A is Knight, what he said it's true. Else, it's false. 
+    # A said one of this statement, but not both: "Aknight", "AKnave"
+    Or(
+        And(
+            Implication(AKnight, AKnight),
+            Implication(AKnave, Not(AKnight))
+        ),
+        And(
+            Implication(AKnight, AKnave),
+            Implication(AKnave, Not(AKnave))
+        ),
+    ),
+
+    # Encode the knowledge that if B is Knight, what he said it's true. Else, it's false. 
+    # B said: "And(Implication(AKnight, AKnight), Implication(AKnave, Not(AKnight)))"
+    Implication(BKnight, And(Implication(AKnight, AKnight), Implication(AKnave, Not(AKnight)))),
+    Implication(BKnave, Not(And(Implication(AKnight, AKnight), Implication(AKnave, Not(AKnight))))),
+    # B also said: "CKnave"
+    Implication(BKnight, CKnave),
+    Implication(BKnave, Not(CKnave)),
+
+    # Encode the knowledge that if C is Knight, what he said it's true. Else, it's false. 
+    # C said: "Aknight"
+    Implication(CKnight, AKnight),
+    Implication(CKnave, Not(AKnight))
+)
 
 def main():
     symbols = [AKnight, AKnave, BKnight, BKnave, CKnight, CKnave]
