2fcd085dbccdf5abe94edfa44b95e5ba20d37932
AndreaDiNapoli <andrea.dinapoli.90@gmail.com>
2021-10-24 18:42:25 +0200

    Pagerank completed

--- pagerank/pagerank.py
+++ pagerank/pagerank.py
@@ -2,6 +2,7 @@ import os
 import random
 import re
 import sys
+import copy
 
 DAMPING = 0.85
 SAMPLES = 10000
@@ -61,16 +62,16 @@ def transition_model(corpus, page, damping_factor):
     distr_probability = {}
 
     # Check if the input page have any link. If no, then return an evenly distribution for each page in the corpus (random selection)
-    if corpus[page] == None:
+    if not corpus[page]:
         for pages in corpus:
             distr_probability[pages] = 1/len(corpus)
         return distr_probability
 
-    # Calculate the probability for each link in the page, as pr_randomlink = P(link | Damping_factor)
-    pr_randomlink = ( ( 1/len(corpus[page]) ) + damping_factor ) / damping_factor
+    # Calculate the probability for each link in the page (df/number of possible links)
+    pr_randomlink = damping_factor / len(corpus[page])
 
-    # Calculate the probability for each page in the corpus, as pr_randompage = P(page | (1-Damping_factor))
-    pr_randompage = ( ( 1/len(corpus) ) + (1 - damping_factor ) ) / (1 - damping_factor ) 
+    # Calculate the probability for each page in the corpus (1-df/number of pages in corpus)
+    pr_randompage = (1 - damping_factor) / len(corpus)
 
     # Loop for every page and build the dictionary
     for pages in corpus:
@@ -98,7 +99,8 @@ def sample_pagerank(corpus, damping_factor, n):
     samples = []
 
     # Create a first sample by random and add it to a list
-    sample = random.choice(corpus)
+    pagelist = list(corpus.keys())
+    sample = random.choice(pagelist)
     samples.append(sample)
 
     # Loop for n-1 samples
@@ -106,14 +108,15 @@ def sample_pagerank(corpus, damping_factor, n):
         # Call the transition model for the current sample and check the probability distribution
         distr_probability = transition_model(corpus, sample, damping_factor)
         # Extract a random page from the corpus, accordingly to the probability distribution
-        sample = random.choice(distr_probability, list(distr_probability.values()))
+        page_keys = list(distr_probability.keys())
+        sample = random.choices(page_keys, weights = list(distr_probability.values()), k=1)[0]
         # Add the page to the list
         samples.append(sample)
 
     # Loop through the list and count every page, adding 1 to the dictionary
     for sample in samples:
         if sample in pageranks:
-            pageranks[sample] +=1
+            pageranks[sample] += 1
         else:
             pageranks[sample] = 1
 
@@ -122,6 +125,25 @@ def sample_pagerank(corpus, damping_factor, n):
         pageranks[page] = pageranks[page] / n
     # Return the solution
     return pageranks
+
+def pagerankFormula(pagerank, damping_factor, corpus):
+    """
+    Applicate the pagerank formula
+    """
+    # Create e deepcopy of the dictionary
+    new_pagerank = copy.deepcopy(pagerank)
+
+    # Loop for every page in the corpus and calculate a new value of the pagerank using the formula, store them in the new dictionary copy
+    for page in new_pagerank:
+        # Calculate the summatory of pr(i)/numlink(i), where i is every page that link to the page considered in this loop step
+        sum = 0
+        for i in corpus:
+            if page in corpus[i]:
+                sum += ( pagerank[i] / len(corpus[i]) )
+        # Applicate the formula and update the value
+        new_pagerank[page] = ( (1 - damping_factor)/len(corpus) ) + (damping_factor * sum)
+    
+    return new_pagerank
 
 
 def iterate_pagerank(corpus, damping_factor):
@@ -133,7 +155,33 @@ def iterate_pagerank(corpus, damping_factor):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+    # Declare an empty dictionary
+    pageranks2 = {}
+    # Loop for every page in the corpus, and create a dict key for that page, with a starting pagerank value of 1/pages
+    for page in corpus:
+        pageranks2[page] = 1/len(corpus)
+
+    # Declare a controller value
+    delta_controller = False
+
+    while delta_controller == False:
+
+        # Calculate the pageranks using the iterative formula until convergence
+        new_pageranks2 = pagerankFormula(pageranks2, damping_factor, corpus)
+
+        # Loop throug every page in the dictionary and calculate the delta from the old dict values
+        delta = []
+        for page in new_pageranks2:
+            # Calculate the difference in value (and turn to positive)
+            delta.append(abs(pageranks2[page] - new_pageranks2[page]))
+    
+        pageranks2 = new_pageranks2
+
+        # Check if all the delta is under 0.001, if yes, set the controller to true
+        if all(i < 0.001 for i in delta):
+            delta_controller = True
+    
+    return new_pageranks2
 
 
 if __name__ == "__main__":
